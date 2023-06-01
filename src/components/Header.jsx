98400936e5c2456ccd4d55f94d894e355b9b991b
Victor Ike <victorike08@gmail.com>
2023-05-31 21:17:22 -0500

    Create Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import memeLogo from '../assets/meme-logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={memeLogo} alt="Meme Icon" />
+    </header>
+  );
+}
---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <main>
+      <form action="#">
+        <input type="text" placeholder="Top Section" />
+        <input type="text" placeholder="Buttom Section" />
+        <input type="submit" value="Get a new meme image 🌁" />
+      </form>
+    </main>
+  );
+}
