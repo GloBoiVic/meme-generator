2de7e4de3725f4b52437ae622945262165e4a450
Victor Ike <victorike08@gmail.com>
2023-06-09 15:08:05 -0500

    Update contents

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import memesData from "../memesData";
+
+// console.log(memesData.data.memes);
+
 export default function Meme() {
+  const [meme, setMemeImage] = useState("https://i.imgflip.com/1g8my4.jpg");
+
+  const getImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  };
   return (
     <main>
       <form action="#">
         <input type="text" placeholder="Top Section" />
         <input type="text" placeholder="Buttom Section" />
-        <input type="submit" value="Get a new meme image 🌁" />
+        <input
+          type="submit"
+          onClick={getImage}
+          className="submit"
+          value="Get a new meme image 🌁"
+        />
       </form>
+
+      <div className="image">
+        <img src={meme} alt="" />
+      </div>
     </main>
   );
 }
